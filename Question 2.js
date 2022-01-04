41995f521bc2fd96c29b009fd4bbd63b3fcebced
Sathyamoorthi17 <85884448+Sathyamoorthi17@users.noreply.github.com>
2022-01-04 13:50:30 +0530

    Create Question 2.js

---
+++ Question 2.js
@@ -0,0 +1,45 @@
+// resume data in JSON format
+
+{
+    "basics" : {
+        "name" : "Sathyamoorthi G" ,
+        "degree" : "B.SC Computer Science",
+        "contact" : "9876543210",
+        "e-mail" : "sathyamoorthi@gmail.com",
+        "gender" : "Male",
+        "dob" : "17/09/1999",
+        "location" : {
+            "address" : "133/B pudukuyyanur",
+            "city" : "sathyamangalam",
+            "districts" : "Erode",
+            "pincode" : "638401"
+        }
+    },
+    "education in SSLC" : {
+        "Institution" : "Sri Ragavendra Higher Secondary School",
+        "Board" : "State Board",
+        "year of Passing" : "2015",
+        "Percentage" : "87%"
+    },
+    "education in Higher Secondary" : {
+        "Institution" : "Sri Ragavendra Higher Secondary School",
+        "Board" : "State Board",
+        "year of Passing" : "2017",
+        "Percentage" : "71%"
+    },
+    "education in B.Sc computer science" : {
+        "Institution" : "Dr.SNS Rajalakshmi College Of Arts and Science",
+        "Board" : "Bharathiyar University",
+        "year of Passing" : "2020",
+        "Percentage" : "72%"
+    },
+    "Others" : {
+        "area of interest" : "Web Development",
+        "co-curricular activities" : "Attend Mobile Application development Workshop",
+        "Positions of Responsibilities" : "One of the coordinator in game fest at DRSNRCAS in computer science",
+        "strengths" : "Hard working and have good interpersonal skills",
+        "hobbies" : "playing chess and"
+    }
+
+
+}
