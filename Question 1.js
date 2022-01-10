ce3ddc6f47d9976d916fabd4b11a38e98e7f448a
Sathyamoorthi17 <85884448+Sathyamoorthi17@users.noreply.github.com>
2022-01-10 19:14:24 +0530

    Update Question 1.js

--- Question 1.js
+++ Question 1.js
@@ -1,1 +1,27 @@
-// JSON iterate over all for loops (for, for in, for of, forEach)
+// How to compare two JSON have the same properties without order?
+// a. var obj1 = { name: "Person 1", age:5 };
+// b. var obj2 = { age:5, name: "Person 1" };
+
+
+var obj1 = { name: "Person 1", age:5 };
+var obj2 = { age:5, name: "Person 1" };
+
+var flag=true;
+
+if (Object.keys(obj1).length==Object.keys(obj2).length) {
+    for(key in obj1) { 
+        if(obj1[key] == obj2[key]) {
+            continue;
+        }
+        else {
+            flag=false;
+            break;
+        }
+    }
+}
+else {
+    flag=false;
+}
+console.log(flag);
+
+// Output : true
